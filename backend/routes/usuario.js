344e2baa0648dd62a7f413ead052311e8377105e
LaraYslen-07 <yslenlaragb@gmail.com>
2025-12-15 14:37:29 -0300

    Temporarily remove the obterUsuario route to fix deploy

--- backend/routes/usuario.js
+++ backend/routes/usuario.js
@@ -8,9 +8,6 @@ router.post('/', upload.single('foto'), usuarioController.criarUsuario);
 // Adicione essa linha:
 router.post('/login', usuarioController.loginUsuario);
 
-// Rota GET para obter usuario por ID
-router.get('/:id', usuarioController.obterUsuario);
-
 // Rota GET para listar usuarios
 router.get('/', usuarioController.listarUsuarios);
 
